d09afd0fa3ce3a2a48db722eaf96725b6b991038
RodBrowning <rodrigo.browning@gmail.com>
2022-07-08 17:04:07 -0300

    Inicial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import './App.scss';
+import { BsCurrencyExchange } from 'react-icons/bs';
 
 function App() {
   return (
     <div className="App">
-      <p>Testando sass</p>
+      <div className="main-container">
+        <div className="heading">
+          <h1>Consersor de valores</h1>
+          <h6>Dolar: R$5,00</h6>
+        </div>
+        <div className="main-panel">
+          <div className="currency-panel">
+            <select name="" id="">
+              <option value="tes">tes</option>
+              <option value="tes">tes2</option>
+            </select>
+            <input type="text" />
+          </div>
+          <BsCurrencyExchange className="exchange-icon" />
+          <div className="currency-panel">
+            <select name="" id="">
+              <option value="tes">tes</option>
+            </select>
+            <input type="text" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
