448b09858e03145347665685194d012a3fa5f1f5
RodBrowning <rodrigo.browning@gmail.com>
2022-07-23 21:02:28 -0300

    Panel Style

--- src/Components/CurrencyDisplay/index.tsx
+++ src/Components/CurrencyDisplay/index.tsx
@@ -27,8 +27,10 @@ const CurrencyDisplay: React.FC<Props> = ({
   inicialCurrencySymbol,
 }) => {
   const inputText = useRef<HTMLInputElement>(null);
+  const [showInput, setShowInput] = useState<boolean>(false);
   const [value, setValue] = useState<string>('0.00');
   const [displayValue, setDisplayValue] = useState<string>('');
+  const [currencyTitle, setCurrencyTitle] = useState<string>();
   const [currencySymbol, setCurrencySymbol] = useState<string>('USD');
   const [currencyVal, setCurrencyVal] = useState<number>(1);
   const [symbolsAndTitles, setSymbolsAndTitles] = useState<[string, string][]>([]);
@@ -43,7 +45,7 @@ const CurrencyDisplay: React.FC<Props> = ({
     return navigator && navigator.languages && navigator.languages.length ? navigator.languages[0] : navigator.language;
   };
 
-  const currencyFormatter = Intl.NumberFormat(userLocale() || 'pt-BR', {
+  const currencyFormatter = Intl.NumberFormat(userLocale() || 'en-US', {
     style: 'currency',
     currency: currencySymbol,
     useGrouping: true,
@@ -75,7 +77,7 @@ const CurrencyDisplay: React.FC<Props> = ({
     if (!valid) return;
 
     setValue(val);
-    setDisplayValue(val);
+    setDisplayValue(currencyFormatter.format(Number(val)));
     sendNewDolarAmountToParent(Number(val));
   };
 
@@ -86,7 +88,6 @@ const CurrencyDisplay: React.FC<Props> = ({
 
   useEffect(() => {
     const inicialCurrencyVal = currenciesInfo.latest.rates![inicialCurrencySymbol];
-    setCurrencySymbol(inicialCurrencySymbol);
     setCurrencyVal(inicialCurrencyVal);
     setCurrencySymbol(inicialCurrencySymbol);
     setBgCurrencyHandler(index, inicialCurrencySymbol);
@@ -104,30 +105,59 @@ const CurrencyDisplay: React.FC<Props> = ({
 
   return (
     <div className="currency-panel">
-      <select
-        value={currencySymbol}
-        onChange={(e) => {
-          onSelectChangeHandler(e);
-        }}
-      >
-        {symbolsAndTitles.map(([symbol, title]) => {
-          return <option key={symbol} value={symbol}>{`${symbol} - ${title}`}</option>;
-        })}
-      </select>
-      <input
-        type="text"
-        value={displayValue}
-        ref={inputText}
-        onChange={(e) => {
-          onValueChangeHandler(e.target.value);
-        }}
-        onFocus={() => {
-          setDisplayValue(value);
-        }}
-        onBlur={() => {
-          setDisplayValue(currencyFormatter.format(Number(value)));
-        }}
-      />
+      <div className="selectedCurrency">
+        <h2>{currencyTitle}</h2>
+      </div>
+      <div className="select-wrapper">
+        <select
+          value={currencySymbol}
+          onChange={(e) => {
+            onSelectChangeHandler(e);
+          }}
+        >
+          {symbolsAndTitles.map(([symbol]) => {
+            return <option key={symbol} value={symbol}>{`${symbol}`}</option>;
+          })}
+        </select>
+        <svg width="15" height="9" viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M0.548209 0.944665C1.18041 0.297965 2.06056 0.247215 2.83341 0.944665L8.50001 6.37782L14.1666 0.944665C14.9395 0.247215 15.8211 0.297965 16.4489 0.944665C17.0811 1.58992 17.0405 2.68032 16.4489 3.28642C15.8602 3.89252 9.64116 9.81431 9.64116 9.81431C9.49306 9.9678 9.31557 10.0899 9.11925 10.1733C8.92294 10.2566 8.71184 10.2996 8.49856 10.2996C8.28527 10.2996 8.07418 10.2566 7.87787 10.1733C7.68155 10.0899 7.50405 9.9678 7.35596 9.81431C7.35596 9.81431 1.13981 3.89252 0.548209 3.28642C-0.044841 2.68032 -0.083991 1.58992 0.548209 0.944665Z"
+            fill="black"
+            fillOpacity="0.9"
+          />
+        </svg>
+      </div>
+      <div className="input-wrapper">
+        {showInput ? (
+          <input
+            // eslint-disable-next-line jsx-a11y/no-autofocus
+            autoFocus
+            type="text"
+            value={value}
+            ref={inputText}
+            onChange={(e) => {
+              onValueChangeHandler(e.target.value);
+            }}
+            onBlur={() => {
+              setShowInput(false);
+            }}
+            // eslint-disable-next-line consistent-return
+            onKeyDown={(e) => {
+              if (e.code === 'Enter' || e.code === 'NumpadEnter') return e.target.blur();
+            }}
+          />
+        ) : (
+          // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+          <div
+            className="display-value"
+            onClick={() => {
+              setShowInput(true);
+            }}
+          >
+            <p>{displayValue}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
