b1828a805c7b5e2dd5f72d49892b70f92feb46af
RodBrowning <rodrigo.browning@gmail.com>
2022-07-25 14:56:47 -0300

    Add final API URL for production requests

--- src/Utils/fetch.ts
+++ src/Utils/fetch.ts
@@ -3,8 +3,16 @@ import { checkCookie, setCookie } from './cookies';
 import { ILatest } from '../Types/Latest';
 import { ISymbols } from '../Types/Symbols';
 
-const fetchCurrencies = async (path: 'symbols' | 'latest'): Promise<ISymbols | ILatest> => {
-  const response = await fetch(`http://localhost:3004/${path}`);
+const fetchCurrencies = async (pPath: 'symbols' | 'latest'): Promise<ISymbols | ILatest> => {
+  let URL = 'http://localhost:3004/';
+  let path = `${pPath}`;
+  const headers = new Headers();
+  if (import.meta.env.PROD) {
+    URL = import.meta.env.VITE_BASE_API_URL;
+    path = pPath === 'latest' ? `${pPath}?base=USD` : pPath;
+    headers.append('apikey', import.meta.env.VITE_API_KEY);
+  }
+  const response = await fetch(`${URL}${path}`, { headers });
   const data: ISymbols | ILatest = await response.json();
   return data;
 };
