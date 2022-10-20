279f4b9c2f481d353932868f7c886fd21c493a86
RodBrowning <rodrigo.browning@gmail.com>
2022-10-20 09:59:05 -0300

    Code review - prevent multiple useEffect calls in the Flags component

--- src/Components/Flags/index.tsx
+++ src/Components/Flags/index.tsx
@@ -1,6 +1,6 @@
 import './style.scss';
 
-import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import currenciesFlags from './flags';
 
@@ -10,33 +10,31 @@ interface Props {
 
 const Flag: React.FC<Props> = ({ currencySymbol }) => {
   const flagContainerRef = useRef<HTMLDivElement>(null);
-  const [index, setIndex] = useState<number>(0);
   const [flags, setFlags] = useState<string[]>(currenciesFlags[currencySymbol]);
 
   useEffect(() => {
     setFlags([...currenciesFlags[currencySymbol]]);
-    setIndex(0);
   }, [currencySymbol]);
 
   // eslint-disable-next-line consistent-return
-  useLayoutEffect(() => {
-    flagContainerRef!.current!.children[index].classList.add('show');
+  useEffect(() => {
+    let index = 0;
+    flagContainerRef.current?.children[index].classList.add('show');
     if (flags.length > 1) {
-      const timeOut = setTimeout(() => {
-        flagContainerRef!.current!.children[index].classList.remove('show');
+      const interval = setInterval(() => {
+        flagContainerRef.current?.children[index].classList.remove('show');
         if (index < flags.length - 1) {
-          setIndex((oldIndex) => {
-            return oldIndex + 1;
-          });
+          index += 1;
         } else {
-          setIndex(0);
+          index = 0;
         }
+        flagContainerRef.current?.children[index].classList.add('show');
       }, 10000);
       return () => {
-        clearTimeout(timeOut);
+        clearInterval(interval);
       };
     }
-  }, [flags, index]);
+  }, [flags]);
 
   return (
     <div className="flags-container" ref={flagContainerRef}>
